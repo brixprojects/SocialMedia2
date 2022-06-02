fb9bd995e03172c48f4744dc9b4ad1c8575b4cd0
unknown <brix.delute.aicpa@gmail.com>
2022-06-02 17:07:11 +0800

    second attempt

---
+++ src/components/InfoCard/InfoCard.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './InfoCard.css'
+import {UilPen} from '@iconscout/react-unicons'
+
+const InfoCard = () => {
+  return (
+    <div className="InfoCard">
+        <div className="infoHead">
+            <h4>Your Info</h4>
+            <div>
+             <UilPen 
+             width="2rem" 
+             height="1.2rem"/>
+            </div>
+           
+        </div>
+        <div className="info">
+            <span>
+                <b>Status </b>
+            </span>
+            <span>in a Realtionship</span>
+        </div>
+        <div className="info">
+            <span>
+                <b>Lives in </b>
+            </span>
+            <span>Sampaguita </span>
+        </div>
+        <div className="info">
+            <span>
+                <b>Works at </b>
+            </span>
+            <span>Bugtech Company </span>
+        </div>
+
+        <button className="button logout-button">Logout</button>
+    </div>
+  )
+}
+
+export default InfoCard
