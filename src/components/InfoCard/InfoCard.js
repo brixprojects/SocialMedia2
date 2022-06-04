a72c53ce753b802cfba827fc2f6f2a1f2c1871d2
unknown <brix.delute.aicpa@gmail.com>
2022-06-04 11:39:46 +0800

    Complete Features

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './InfoCard.css'
 import {UilPen} from '@iconscout/react-unicons'
+import ProfileModal from "../ProfileModal/ProfileModal"
 
 const InfoCard = () => {
+    const [modalOpened, setModalOpened] = useState(false);
   return (
     <div className="InfoCard">
         <div className="infoHead">
@@ -10,7 +12,13 @@ const InfoCard = () => {
             <div>
              <UilPen 
              width="2rem" 
-             height="1.2rem"/>
+             height="1.2rem"
+             onClick={() => setModalOpened(true)}
+             />
+             <ProfileModal
+             modalOpened={modalOpened}
+             setModalOpened={setModalOpened}
+            />
             </div>
            
         </div>
